49d2f876741f6e57f9acbe7027fcbc8e187febb7
K-aigoro <aigorokazeem90@gmail.com>
2024-11-25 23:55:24 +0100

    Tuples

---
+++ Tuples.py
@@ -0,0 +1,23 @@
+
+# Tuples are used to store multiple items in a single variable.
+mytuple = ("apple", "banana", "cherry")
+print(mytuple)
+
+
+# Unchangeable - it means that items can not be change, add or
+# removed after tuples has been created
+
+# Access a tuple 
+second = mytuple[1]
+print(second)
+
+# Length of a tuple
+print(len(mytuple))
+
+# Tuples can be any datatype
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+tuple_all = ("abc", 10, True, "apple")
+print(tuple_all)
