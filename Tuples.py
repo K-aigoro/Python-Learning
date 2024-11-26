5d7aa5576137ca7a1d6b3ffaf8b86725192f3fb9
K-aigoro <aigorokazeem90@gmail.com>
2024-11-26 23:37:24 +0100

    Update tuples course

--- Tuples.py
+++ Tuples.py
@@ -7,7 +7,7 @@ print(mytuple)
 # Unchangeable - it means that items can not be change, add or
 # removed after tuples has been created
 
-# Access a tuple 
+# Access a tuple
 second = mytuple[1]
 print(second)
 
@@ -21,3 +21,40 @@ tuple3 = (True, False, False)
 
 tuple_all = ("abc", 10, True, "apple")
 print(tuple_all)
+
+
+# check if item exist
+
+if "banana" in tuple1:
+    print("yes")
+
+
+x = list(tuple1)
+x[1] = 5
+print(x)
+tuple1 = tuple(x)
+print(tuple1)
+
+
+# unpacking in tuple
+
+fruit = ("Pineapple", "orange", "Apple", "Carrot")
+(green, yellow, *red) = fruit
+print(green)
+print(red)
+print(yellow)
+
+
+# Loop through a tuple
+
+# using for Loop
+print("This is a loop")
+
+for x in fruit:
+    print(x)
+
+
+print(" join tuple")
+
+tuple1 = + tuple2
+print(tuple1)
